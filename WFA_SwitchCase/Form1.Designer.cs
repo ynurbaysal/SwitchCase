992094221f575c688d8da65f7a66ae69b2943dbb
muratvuranok <mrtvrnk@gmail.com>
2019-07-17 13:58:05 +0300

    Ornek Eklendi

--- WFA_SwitchCase/Form1.Designer.cs
+++ WFA_SwitchCase/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.btnOrnek2 = new System.Windows.Forms.Button();
             this.btnOrnek3 = new System.Windows.Forms.Button();
             this.btnOrnek4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtGelenDeger1
@@ -59,42 +60,57 @@
             this.btnOrnek1.TabIndex = 1;
             this.btnOrnek1.Text = "ONAYLA";
             this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.BtnOrnek1_Click);
             // 
             // btnOrnek2
             // 
             this.btnOrnek2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOrnek2.Location = new System.Drawing.Point(12, 139);
+            this.btnOrnek2.Location = new System.Drawing.Point(12, 202);
             this.btnOrnek2.Name = "btnOrnek2";
             this.btnOrnek2.Size = new System.Drawing.Size(334, 57);
             this.btnOrnek2.TabIndex = 1;
             this.btnOrnek2.Text = "KULLANICI GİRİŞİ";
             this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.BtnOrnek2_Click);
             // 
             // btnOrnek3
             // 
             this.btnOrnek3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOrnek3.Location = new System.Drawing.Point(12, 202);
+            this.btnOrnek3.Location = new System.Drawing.Point(12, 265);
             this.btnOrnek3.Name = "btnOrnek3";
             this.btnOrnek3.Size = new System.Drawing.Size(334, 57);
             this.btnOrnek3.TabIndex = 1;
             this.btnOrnek3.Text = "İÇ İÇE SWITCH";
             this.btnOrnek3.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Click += new System.EventHandler(this.BtnOrnek3_Click);
             // 
             // btnOrnek4
             // 
             this.btnOrnek4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOrnek4.Location = new System.Drawing.Point(12, 265);
+            this.btnOrnek4.Location = new System.Drawing.Point(12, 328);
             this.btnOrnek4.Name = "btnOrnek4";
             this.btnOrnek4.Size = new System.Drawing.Size(334, 57);
             this.btnOrnek4.TabIndex = 1;
             this.btnOrnek4.Text = "C#7 Gelen Özellik";
             this.btnOrnek4.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(334, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ONAYLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 333);
+            this.ClientSize = new System.Drawing.Size(361, 395);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnOrnek4);
             this.Controls.Add(this.btnOrnek3);
             this.Controls.Add(this.btnOrnek2);
@@ -118,6 +134,7 @@
         private System.Windows.Forms.Button btnOrnek2;
         private System.Windows.Forms.Button btnOrnek3;
         private System.Windows.Forms.Button btnOrnek4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WFA_SwitchCase/Form1.cs
+++ WFA_SwitchCase/Form1.cs
@@ -16,5 +16,93 @@ namespace WFA_SwitchCase
         {
             InitializeComponent();
         }
+
+
+        /*
+         * SWITCH - CASE
+         * IF-Else mantigiyle calisan bir kontrol mekanizmasidir. Aradaki fark, if-else yapisi buyukluk-kucukluk gibi durumlari kontrol ederken,
+         * switch-case yalnizca esitlik durumlarini kontrol edebilen karar yapimizdir.. Kendisine ait bir index mekanizmasi ile kosullari algilar, 
+         * siraya koyar ve otomatik olarak yuzlerce kosulunuz olsa bile herbirine bakmadan hangisine uydugunu bulabilir. Bu da performansı direktman  etkiler...
+         * Switch blogu icerisine yazdiginiz veri tipiniz ne ise, case (durum) olarak belirttiginiz tum veriler de ayni veri tipinde olmalıdır!
+         * break => Kosul saglandi, artik karar yapisindan kendini disari atabilirsin mesajini sisteme verir...
+         */
+
+
+        private void BtnOrnek1_Click(object sender, EventArgs e)
+        {
+            string mevsim = txtGelenDeger1.Text.ToLower();
+
+            switch (mevsim)
+            {
+                case "yaz":
+                    MessageBox.Show("Haziran - Temmuz - Ağustos");
+                    break;
+                case "sonbahar":
+                    MessageBox.Show("Eylül - Ekim - Kasım");
+                    break;
+                case "kış":
+                    MessageBox.Show("Aralık - Ocak - Şubat");
+                    break;
+                case "ilkbahar":
+                    MessageBox.Show("Mart - Nisan - Mayıs");
+                    break;
+                default:
+                    MessageBox.Show("Böyle bir mevsim bulunamadı!");
+                    break;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string ay = txtGelenDeger1.Text.ToLower();
+            switch (ay)
+            {
+
+                case "haziran":
+                case "temmuz":
+                case "agustos":
+                    MessageBox.Show("Yaz");
+                    break;
+
+                case "eylül":
+                case "ekim":
+                case "kasım":
+                    MessageBox.Show("Sonbahar");
+                    break;
+
+                case "aralık":
+                case "ocak":
+                case "şubat":
+                    MessageBox.Show("Kış");
+                    break;
+
+
+                case "mart":
+                case "nisan":
+                case "mayıs":
+                    MessageBox.Show("İlkbahar");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void BtnOrnek2_Click(object sender, EventArgs e)
+        {
+            //Eger ilk textbox'a "Admin","Moderator","Yonetici","CEO","Baskan" degerleri girilirse "Yönetim Paneline Yönlendiriliyorsunuz..."
+            //"Uye" girilirse "Ana Sayfaya Yonlendiriliyorsunuz...";
+            //Harici bir durumda "Bu Sayfayı Görme Yetkiniz Yok!";
+        }
+
+        private void BtnOrnek3_Click(object sender, EventArgs e)
+        {
+            //İç içe switch kullanarak kullanıcı adını ve şifresini kontrol ediniz.eğer kullanıcı kullanıcıadını ve şifresini doğru girerse "Tebrikler, hem kullanıcı adı hemde şifreniz doğru" uyarısı
+            //Kullanıcı adı doğru şifre yanlışsa "Kullanıcı adınız doğru ancak şifreniz yanlış!" uyarısı
+            //kullanıcı adı yanlışsa "Kullanıcı adınızı yanlış girdiniz, şifreye bakmadım bile!" uyarusını kullanıcıya veriniz.
+
+            //KullaniciAdi = "bilgeadam";
+            //Sifre = "1234";
+        }
     }
 }
